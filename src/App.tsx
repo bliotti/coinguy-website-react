d37d97be82a78eea3094f9e8cb071626314b770b
Brian Liotti <bliotti@protonmail.com>
2023-09-30 00:01:51 -0400

    guts moved over

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="body" className="flex a-items-center flex-column pa3 fw-n">
+      <div className="container wf">
+        <div className="wf mb3">
+          <h1 className="tc mt5">
+            <a href="/" className="cl-near-black">
+              <h4>bliotti</h4>
+            </a>
+          </h1>
+
+          <div className="flex flex-row j-center">
+            <div className="sw5">
+              <a href="https://github.com/bliotti">
+                <img src="/images/github.svg" alt="" title="my repo" />
+              </a>
+            </div>
+
+            <div className="sw5">
+              <a href="/KEY.txt">
+                <img src="/images/key.svg" alt="" title="PGP key" />
+              </a>
+            </div>
+
+            <div className="sw5">
+              <a href="mailto:dev@coinguy.io">
+                <img src="/images/email.svg" alt="" title="mail" />
+              </a>
+            </div>
+
+            <div className="sw5">
+              <a href="https://t.me/coinguybri">
+                <img
+                  src="/images/telegram.png"
+                  alt=""
+                  title="please don't spam me"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+        <nav className="wf tc f3">
+          <hr />
+          <a className="cl-near-black" href="/">
+            home
+          </a>
+          |
+          <a className="cl-near-black" href="/about/">
+            about
+          </a>
+          |
+          <a className="cl-near-black" href="/bookshelf/">
+            bookshelf
+          </a>
+          |
+          <a className="cl-near-black" href="/bitcoin/">
+            bitcoin
+          </a>
+          <hr />
+        </nav>
+      </div>
     </div>
   );
 }
---
+++ src/content/about/_index.md
@@ -0,0 +1,22 @@
++++
+title = "About"
+template = "about.html"
++++
+
+<h4>About Me</h4>
+<p>
+lorem ipsum dolor sit amet
+</p>
+
+<p>
+lorem ipsum dolor sit amet
+</p>
+
+<p>
+lorem ipsum dolor sit amet
+</p>
+
+<h4>About Site</h4>
+<p> 
+lorem ipsum dolor sit amet <a style="text-decoration: underline;" id="random-link" title="take me somewhere random" target=_blank onclick=getRandomLink()>here</a> lorem ipsum dolor sit amet
+</p>
